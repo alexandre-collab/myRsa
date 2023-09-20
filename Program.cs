66f10ad3b9cfa513982a85aa72cc5f898dcaa85f
Arod Alexandre <arod.alexandre.contact@gmail.com>
2023-09-20 16:49:58 +0200

    Init projet

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Utilisation : program.exe \"Texte à chiffrer\"");
+            return;
+        }
+
+        string plaintext = args[0];
+
+        // Reste du code pour générer les clés et chiffrer le texte
+    }
+}
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ## Getting started / install
 
+
+
 ## Commands
