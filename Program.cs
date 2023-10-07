1239197c5cfbe3091e2b670d322f61d845c86d00
alexandre-collab <arod.alexandre.contact@gmail.com>
2023-10-07 15:16:57 +0200

    Init base

--- Program.cs
+++ Program.cs
@@ -1,19 +1,46 @@
 ﻿using System;
-using System.Security.Cryptography;
-using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
-        if (args.Length != 1)
+        if (args.Length == 0 || args[0].ToLower() == "help")
         {
-            Console.WriteLine("Utilisation : program.exe \"Texte à chiffrer\"");
+            displayHelp();
             return;
         }
 
-        string plaintext = args[0];
+        string commande = args[0].ToLower();
 
-        // Reste du code pour générer les clés et chiffrer le texte
+        switch (commande)
+        {
+            case "keygen":
+                Console.WriteLine("keygen");
+                break;
+            case "crypt":
+                Console.WriteLine("crypt");
+                break;
+            case "decrypt":
+                Console.WriteLine("decrypt");
+                break;
+            default:
+                Console.WriteLine("Erreur : Commande invalide. Utilisez 'help' pour afficher le manuel.");
+                break;
+        }
+    }
+
+        static void displayHelp()
+    {
+        Console.WriteLine("Manuel d'utilisation :");
+        Console.WriteLine("Syntaxe : monRSA <commande> [<clé>] [<texte>] [switchs]");
+        Console.WriteLine("Commande :");
+        Console.WriteLine("keygen : Génère une paire de clé");
+        Console.WriteLine("crypt : Chiffre <texte> pour le clé publique <clé>");
+        Console.WriteLine("decrypt : Déchiffre <texte> pour le clé privée <clé>");
+        Console.WriteLine("help : Affiche ce manuel");
+        Console.WriteLine("Clé : Un fichier qui contient une clé publique monRSA ('crypt') ou une clé privée ('decrypt')");
+        Console.WriteLine("Texte : Une phrase en clair ('crypt') ou une phrase chiffrée ('decrypt')");
+        Console.WriteLine("Switchs : -f <file> permet de choisir le nom des clés générées, monRSA.pub et monRSA.priv par défaut");
+        Console.WriteLine("Contrôlez que les paramètres sont corrects.");
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # myRsa Arod Alexandre with c# .NET :)
 
-## Getting started / install
+## Getting started / build & install / Use
 
+### Build & install
+Require .net sdk for be builded 7 :
+https://dotnet.microsoft.com/en-us/download/dotnet/thank-you/sdk-7.0.401-windows-x64-installer
+dotnet build
+
+### Use
+Once the project builder and the binary is created, you can use it with the following command :
 
 
 ## Commands
